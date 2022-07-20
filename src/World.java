261e33381bff8d62b7bec321cbd5b378239a879d
calingoia2001 <calingoia01@yahoo.com>
2022-07-20 16:57:01 +0300

    TSPGene.java

--- src/World.java
+++ src/World.java
@@ -1,2 +1,41 @@
-public class World {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class World extends JPanel {
+    private final AtomicInteger generation;
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+
+    private World() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setBackground(Color.BLACK);
+        this.generation = new AtomicInteger(0);
+        final Timer timer = new Timer(5, (ActionEvent e) ->{
+           repaint();
+        });
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(final Graphics graphics) {
+        super.paintComponent(graphics);
+        final Graphics2D g = (Graphics2D) graphics;
+        g.setColor(Color.CYAN);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.drawString("gen : " + this.generation.incrementAndGet(), 350, 15);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            final JFrame frame = new JFrame("TSB");
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setResizable(false);
+            frame.add(new World(), BorderLayout.CENTER);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
 }
